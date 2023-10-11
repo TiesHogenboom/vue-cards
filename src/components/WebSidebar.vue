a94227054f2af02f34ae689dd76eaa02ffa99077
TiesHogenboom <tieshogenboom03@gmail.com>
2023-10-11 15:04:57 +0200

    improved sidebar to have links inside of links

--- src/components/WebSidebar.vue
+++ src/components/WebSidebar.vue
@@ -2,37 +2,51 @@
   <nav class="container">
     <ul class="list">
       <li class="list-home" @click="toggleAccordion(0)">
-        <RouterLink :to="{ name: 'home' }"><p class="list-home-text">Home</p></RouterLink>
+        <RouterLink :to="{ name: 'home' }">
+          <p class="list-home-text">Home</p>
+        </RouterLink>
       </li>
       <li class="accordion-item" @click="toggleAccordion(1)">
-        <RouterLink :to="{ name: 'cards1' }">Cards 1
-        <div class="accordion-content" :class="{ 'open': isOpen === 1 }">
-          Additional text for Cards 1.
-        </div></RouterLink>
+        <h1>Cards</h1>
+          <div class="accordion-content" :class="{ 'open': isOpen === 1 }">
+
+            <li class="accordion-item-child" @click="toggleAccordion(2)">
+            <RouterLink :to="{ name: 'cards1' }">Cards 1
+              <div class="accordion-content-child" :class="{ 'open': isOpen === 2 }">
+                test
+              </div>
+            </RouterLink>
+          </li>
+          <li class="accordion-item-child" @click="toggleAccordion(3)">
+            <RouterLink :to="{ name: 'cards2' }">Cards 2
+              <div class="accordion-content-child" :class="{ 'open': isOpen === 3 }">
+                <p>Additional text for Cards 2.</p>
+              </div>
+            </RouterLink>
+          </li>
+
+          </div>
       </li>
-      <li class="accordion-item" @click="toggleAccordion(2)">
-        <RouterLink :to="{ name: 'cards2' }">Cards 2
-        <div class="accordion-content" :class="{ 'open': isOpen === 2 }">
-          Additional text for Cards 2.
-        </div></RouterLink>
-      </li>
-      <li class="accordion-item" @click="toggleAccordion(3)">
+      <li class="accordion-item" @click="toggleAccordion(4)">
         <RouterLink :to="{ name: 'createcard' }">Create card
-        <div class="accordion-content" :class="{ 'open': isOpen === 3 }">
-          Additional text for Create card.
-        </div></RouterLink>
+          <div class="accordion-content" :class="{ 'open': isOpen === 4 }">
+            Additional text for Create card.
+          </div>
+        </RouterLink>
       </li>
       <li class="accordion-item" @click="toggleAccordion(4)">
         <RouterLink :to="{ name: 'about' }">About
-        <div class="accordion-content" :class="{ 'open': isOpen === 4 }">
-          Additional text for About.
-        </div></RouterLink>
+          <div class="accordion-content" :class="{ 'open': isOpen === 4 }">
+            Additional text for About.
+          </div>
+        </RouterLink>
       </li>
       <li class="accordion-item" @click="toggleAccordion(5)">
         <RouterLink :to="{ name: 'todo' }">To do
-        <div class="accordion-content" :class="{ 'open': isOpen === 5 }">
-          Additional text for To do.
-        </div></RouterLink>
+          <div class="accordion-content" :class="{ 'open': isOpen === 5 }">
+            Additional text for To do.
+          </div>
+        </RouterLink>
       </li>
     </ul>
   </nav>
@@ -87,14 +101,14 @@ a {
   font-weight: 700;
 }
 
-.list-home{
-  margin-top:40px;
+.list-home {
+  margin-top: 40px;
   padding-bottom: 40px;
   list-style-type: none;
-  
+
 }
 
-.list-home-text{
+.list-home-text {
   width: 80px;
   height: 30px;
   transition: 0.2s;
@@ -109,7 +123,13 @@ a {
   height: 32px;
 }
 
-.accordion-item{
+.accordion-content-li {
+  list-style-type: none;
+  margin-bottom: 5px;
+  ;
+}
+
+.accordion-item {
   list-style-type: none;
   margin-bottom: 20px;
   cursor: pointer;
@@ -129,11 +149,30 @@ a {
   font-weight: 400;
 }
 
+.accordion-content-child {
+  max-height: 0;
+  overflow: hidden;
+  margin-top: 10px;
+  font-size: 16px;
+  padding-left: 10px;
+  background-color: white;
+  border-left: 2px solid black;
+  z-index: 1;
+  transition: max-height 0.3s ease;
+  font-weight: 400;
+}
+
+
 .open {
   max-height: 500px;
 }
 
 .accordion-item:hover .accordion-content {
+  display: block;
+  max-height: 500px;
+}
+
+.accordion-item-child:hover .accordion-content-child {
   display: block;
   max-height: 500px;
 }
