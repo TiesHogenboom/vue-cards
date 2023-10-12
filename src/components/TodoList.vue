b2d4bda3fe11ee6db8141b3c6d512744c1bc9857
TiesHogenboom <tieshogenboom03@gmail.com>
2023-10-12 11:02:58 +0200

    finished styling todo cards

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -22,8 +22,8 @@
             <h1 class="title">Account</h1>
             <ul class="list">
                 <li>Create login form</li>
-                <li>create register form</li>
-                <li>create accept register page?</li>
+                <li>Create register form</li>
+                <li>Create accept register page?</li>
                 <li>Create account page</li>
             </ul>
         </div>
@@ -50,15 +50,28 @@
     width: 300px;
     height: 400px;
     background: white;
-    border: 3px solid black;
+    border: 3px solid rgb(209, 209, 227);
+    border-right: 5px solid rgb(191, 191, 214);
+    border-top: 4px solid rgb(191, 191, 214);
+    box-shadow: 5px -4px 1px 0 rgba(191, 191, 214);
     border-radius: 10px;
     margin: 10px;
+    transition: 0.4s;
+}
+
+.card:hover{
+    padding: 11px;
+    border: 2px solid rgb(214, 214, 233);
+    border-right: 4px solid rgb(199, 199, 222);
+    border-top: 3px solid rgb(199, 199, 222);
+    box-shadow: 4px -3px 1px 0 rgb(199, 199, 222);
 }
 
 .title {
     display: flex;
     justify-content: center;
     padding-top: 20px;
+    color: rgb(209, 209, 227);
 }
 
 .list {
@@ -67,8 +80,10 @@
 }
 
 li {
-    border-top: 2px solid lightgray;
+    border-top: 2px solid rgb(209, 209, 227);;
     padding-top: 4px;
     padding-bottom: 4px;
+    font-size: 18px;
+    font-weight: 400;
 }
 </style>
