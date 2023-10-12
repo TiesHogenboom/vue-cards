4a16fdeebc9023557070616842472584fb7b9230
TiesHogenboom <tieshogenboom03@gmail.com>
2023-10-12 10:02:02 +0200

    Added basic styling to todo cards

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,33 @@
 <template>
-    <div class="container">   
-        <h1>Some things to do</h1>
-        <ul class="textstyle">
-            <li>Add text to Todo</li>
-            <li>Add styling to Todo</li>
-            <li>Create more unique cards</li>
-            <li>Make create card page</li>
-            <li>Add input in to do list</li>
-        </ul>
+    <div class="container">
+
+        <div class="card">
+            <h1 class="title">To do</h1>
+            <ul class="list">
+                <li>Add text to Todo</li>
+                <li>Add styling to Todo</li>
+                <li>Add input in to do list</li>
+            </ul>
+        </div>
+
+        <div class="card">
+            <h1 class="title">Cards</h1>
+            <ul class="list">
+                <li>Create more unique cards</li>
+                <li>Make create card page</li>
+            </ul>
+        </div>
+
+        <div class="card">
+            <h1 class="title">Account</h1>
+            <ul class="list">
+                <li>Create login form</li>
+                <li>create register form</li>
+                <li>create accept register page?</li>
+                <li>Create account page</li>
+            </ul>
+        </div>
+
     </div>
 </template>
 
@@ -17,13 +37,37 @@
 
 
 <style scoped>
-.container{
+.container {
     background: lavender;
-    width: 80vw;
-    height: 100vh;
+    display: flex;
+    flex: 1;
+    padding: 50px;
+    flex-wrap: wrap;
 }
-.textstyle{
-    margin-left: 20px;
-    list-style: armenian;
+
+.card {
+    width: 300px;
+    height: 400px;
+    background: white;
+    border: 3px solid black;
+    border-radius: 10px;
+    margin: 10px;
+}
+
+.title {
+    display: flex;
+    justify-content: center;
+    padding-top: 20px;
+}
+
+.list {
+    margin: 20px;
+    list-style: none;
+}
+
+li {
+    border-top: 2px solid lightgray;
+    padding-top: 4px;
+    padding-bottom: 4px;
 }
 </style>
